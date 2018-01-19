3f42d25771c38543d44901e6849788f90cb9a3f7
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-01-18 18:20:17 -0800

    CRUD update

--- read_with_me/app/controllers/storyboards_controller.rb
+++ read_with_me/app/controllers/storyboards_controller.rb
@@ -16,13 +16,21 @@ class StoryboardsController < ApplicationController
         redirect_to storyboards_path
     end
 
+    def edit
+        @storyboard = Storyboard.find(params[:id])
+    end
+
+    def update
+        @storyboard = Storyboard.find(params[:id])
+        @storyboard.update_attributes(params.require(:storyboard).permit(:name))
+        redirect_to storyboards_path
+    end
+
     def destroy
         storyboard = Storyboard.find(params[:id])
         storyboard.destroy
         redirect_to storyboards_path
     end
 
-
-  
 
 end
