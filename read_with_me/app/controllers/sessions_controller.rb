2c46c9d6d703f7133e886e25e9a3252246b1e531
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-01-18 23:38:11 -0800

    styling/method commit

--- read_with_me/app/controllers/sessions_controller.rb
+++ read_with_me/app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
     def destroy
         session[:user_id] = nil
-        redirect_to root_path, notice: "You have successfully logged out."
+        redirect_to root_path
     end
 
 end
